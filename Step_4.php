8d47117f65d937c643ed3fbb830f9834a519a58a
ahazimeh <hazimeh95@gmail.com>
2020-10-06 11:31:07 +0300

    Step 4

---
+++ Step_4.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Ali";
+$position = "Web Developer";
+$github_url = "https://github.com/ahazimeh/";
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url;
+echo "<br>Hello, I'm $name, I'm a $position please check my github link $github_url";
+echo '<br>Hello, I\'m '.$name.', I\'m a '.$position.' please check my github link '.$github_url;
+?>
