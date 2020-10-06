cfc2119ed99e11f65b76e156c4b85d54823812f5
ahazimeh <hazimeh95@gmail.com>
2020-10-06 12:16:42 +0300

    Step_8

---
+++ Step_8.php
@@ -0,0 +1,5 @@
+<?php
+$array =["Egg","Milk","Cheese","Water","Pack","Tissues","Watermelon"];
+echo "Hello Sir, do you have $array[0], $array[1], and $array[2]? Also if you sell fruits can I find a ".$array[count($array)-1]."?";
+
+?>
