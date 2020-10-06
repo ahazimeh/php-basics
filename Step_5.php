b937fd42585244f34b5ad0d8b083e953948633a1
ahazimeh <hazimeh95@gmail.com>
2020-10-06 11:58:16 +0300

    Step 5

---
+++ Step_5.php
@@ -0,0 +1,10 @@
+<?php
+$course_name = "pHp";
+$enrolled_students = 20;
+$price = 30.0;
+$onDiscount = true;
+echo "Course title: $course_name<br>Enrolled Students: $enrolled_students<br>Course price: $price $ USD<br>Course on discount: ";
+if($onDiscount == true)
+echo "Yes";
+else echo "No";
+?>
