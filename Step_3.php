0884266320deadf947bfd3d42c357ac3dddb5849
ahazimeh <hazimeh95@gmail.com>
2020-10-06 11:21:09 +0300

    Step 3

---
+++ Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Ali";
+$email = "hazimeh95@gmail.com";
+echo "My name is $name and my email is $email";
+$height = 10;
+$width = 15;
+$area = $width*$height;
+echo "<br>Area = $area";
+?>
