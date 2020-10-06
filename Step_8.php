0e2d6188dacb5d4d33342bc7fefb6163634d1083
ahazimeh <hazimeh95@gmail.com>
2020-10-06 12:52:13 +0300

    Step_8

--- Step_8.php
+++ Step_8.php
@@ -1,5 +1,6 @@
 <?php
 $array =["Egg","Milk","Cheese","Water","Pack","Tissues","Watermelon"];
 echo "Hello Sir, do you have $array[0], $array[1], and $array[2]? Also if you sell fruits can I find a ".$array[count($array)-1]."?";
-
+$arraym = array("eggs"=>array("balade","mazere3"),"milk"=>array("Fresh","Taanayel"),"water-pack"=>array("Tanoureen","Reem"));
+echo "<br>Hey Sir, Please I need 1 pack of ".$arraym["eggs"][0]." eggs and 3 ".$arraym["water-pack"][1]." Water Pack";
 ?>
